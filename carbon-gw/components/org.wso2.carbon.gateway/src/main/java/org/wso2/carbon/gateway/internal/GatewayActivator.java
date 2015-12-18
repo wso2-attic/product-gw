27c47484638fe21b62369626bea6c759afb5ab36
Senduran <bsenduran@gmail.com>
2015-12-18 17:50:03 +0530

    sender service reference added

--- carbon-gw/components/org.wso2.carbon.gateway/src/main/java/org/wso2/carbon/gateway/internal/GatewayActivator.java
+++ carbon-gw/components/org.wso2.carbon.gateway/src/main/java/org/wso2/carbon/gateway/internal/GatewayActivator.java
@@ -51,6 +51,7 @@ public class GatewayActivator implements BundleActivator {
             CamelMediationEngine engine = component.getEngine();
 
             bundleContext.registerService(CarbonMessageProcessor.class, engine, null);
+            GatewayContextHolder.getInstance().addMessageProcessor(engine);
 
         } catch (Exception exception) {
             String msg = "Error while loading " + CAMEL_CONTEXT_CONFIG_FILE + " configuration file";
---
+++ carbon-gw/components/org.wso2.carbon.gateway/src/main/java/org/wso2/carbon/gateway/internal/GatewayServiceComponent.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.gateway.internal;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.messaging.TransportSender;
+
+
+/**
+ * Service component for gate way
+ */
+@Component(
+        name = "org.wso2.carbon.gateway.internal.GatewayServiceComponent",
+        immediate = true
+)
+public class GatewayServiceComponent {
+    @Reference(
+            name = "transport-sender",
+            service = TransportSender.class,
+            cardinality = ReferenceCardinality.OPTIONAL,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "removeTransportSender"
+    )
+    protected void addTransportSender(TransportSender transportSender) {
+        GatewayContextHolder.getInstance().addTransportSender(transportSender);
+    }
+
+    protected void removeTransportSender(TransportSender transportSender) {
+        GatewayContextHolder.getInstance().removeTransportSender(transportSender);
+    }
+
+}
