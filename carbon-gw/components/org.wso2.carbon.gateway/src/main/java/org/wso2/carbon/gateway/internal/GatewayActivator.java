974a688d95a83795a383ed35f2e99a77da3c8724
Senduran <bsenduran@gmail.com>
2016-01-18 14:04:28 +0530

    hot deployment removing routes, when removing file changes added, classes refactored

--- carbon-gw/components/org.wso2.carbon.gateway/src/main/java/org/wso2/carbon/gateway/internal/GatewayActivator.java
+++ carbon-gw/components/org.wso2.carbon.gateway/src/main/java/org/wso2/carbon/gateway/internal/GatewayActivator.java
@@ -15,7 +15,6 @@
 
 package org.wso2.carbon.gateway.internal;
 
-import org.apache.camel.model.RoutesDefinition;
 import org.apache.camel.spring.SpringCamelContext;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -23,16 +22,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.wso2.carbon.gateway.internal.mediation.camel.CamelConfigWatchAgent;
+import org.wso2.carbon.gateway.internal.mediation.camel.CamelCustomRouteManager;
 import org.wso2.carbon.gateway.internal.mediation.camel.CamelMediationComponent;
 import org.wso2.carbon.gateway.internal.mediation.camel.CamelMediationEngine;
 import org.wso2.carbon.messaging.CarbonMessageProcessor;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Paths;
 
 /**
  * OSGi Bundle Activator of the gateway Carbon component.
@@ -40,19 +36,20 @@ import java.nio.file.Paths;
 public class GatewayActivator implements BundleActivator {
 
     private static final Logger log = LoggerFactory.getLogger(GatewayActivator.class);
-    private SpringCamelContext camelContext;
-    private ApplicationContext applicationContext;
 
+    public static final String CAMEL_CONTEXT_CONFIG = "camel.config";
+    public static final String CAMEL_CONTEXT_NAME = "camel-context.xml";
     public static final String CAMEL_CONTEXT_CONFIG_FILE =
-            "conf" + File.separator + "camel" + File.separator + "camel-context.xml";
+            "conf" + File.separator + "camel" + File.separator + CAMEL_CONTEXT_NAME;
 
     public static final String CAMEL_CONFIGS_DIRECTORY = "conf" + File.separator + "camel";
 
     public void start(BundleContext bundleContext) throws Exception {
         try {
             SpringCamelContext.setNoStart(true);
-            applicationContext = new ClassPathXmlApplicationContext(new String[] { CAMEL_CONTEXT_CONFIG_FILE });
-            camelContext = (SpringCamelContext) applicationContext.getBean("wso2-cc");
+            String camelDefaultConfigFile = System.getProperty(CAMEL_CONTEXT_CONFIG, CAMEL_CONTEXT_CONFIG_FILE);
+            ApplicationContext applicationContext = new ClassPathXmlApplicationContext(camelDefaultConfigFile);
+            SpringCamelContext camelContext = (SpringCamelContext) applicationContext.getBean("wso2-cc");
             camelContext.start();
             CamelMediationComponent component = (CamelMediationComponent) camelContext.getComponent("wso2-gw");
 
@@ -61,11 +58,12 @@ public class GatewayActivator implements BundleActivator {
             bundleContext.registerService(CarbonMessageProcessor.class, engine, null);
 
             // Add the routes from the custom routes config files in the repository/conf/camel directory
-            log.info("Adding routes from custom routes configuration files ...");
-            addRoutesToContext(CAMEL_CONFIGS_DIRECTORY);
+            CamelCustomRouteManager camelCustomRouteManager = new CamelCustomRouteManager(camelContext);
+
+            camelCustomRouteManager.addRoutesToContext(CAMEL_CONFIGS_DIRECTORY);
 
             // Start the watch service for the custom route file modification in the repository/conf/camel directory
-            new CamelConfigWatchAgent().startWatchingForModifications(Paths.get(CAMEL_CONFIGS_DIRECTORY), this);
+            new CamelConfigWatchAgent().startWatchingForModifications(camelCustomRouteManager);
 
         } catch (Exception exception) {
             String msg = "Error while loading " + CAMEL_CONTEXT_CONFIG_FILE + " configuration file";
@@ -78,53 +76,4 @@ public class GatewayActivator implements BundleActivator {
 
     }
 
-    /**
-     * Adding the routes from custom configuration files
-     *
-     * @param routeConfig custom configuration file
-     */
-    public void addRoutesFromCustomConfigs(File routeConfig) throws FileNotFoundException {
-        log.info("Adding Custom Routes from [" + routeConfig.getName() + "]");
-        if (routeConfig.getPath().equals(CAMEL_CONTEXT_CONFIG_FILE)) {
-            log.warn("Skipping camel Context file [" + routeConfig.getName() + "]");
-            return;
-        }
-
-        InputStream inputStream = new FileInputStream(routeConfig.getAbsolutePath());
-        try {
-            RoutesDefinition routesDefinition = camelContext.loadRoutesDefinition(inputStream);
-            camelContext.addRouteDefinitions(routesDefinition.getRoutes());
-        } catch (Exception e) {
-            String msg = "Error while adding the routes from file [" + routeConfig.getName() + "] ";
-            log.error(msg + e);
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                String msg = "Error while Closing the input stream";
-                log.error(msg + e);
-            }
-        }
-    }
-
-    /**
-     * Adding the routes from the files listed under the directory given by the path
-     *
-     * @param path location of the particular directory
-     */
-    private void addRoutesToContext(String path) throws FileNotFoundException {
-        File directory = new File(path);
-        if (!directory.isDirectory()) {
-            log.warn("Specified path is not a Directory");
-            return;
-        }
-
-        File[] files = directory.listFiles();
-        // Iterate over the files in the specified directory and load routes from them
-        if (files != null) {
-            for (File file : files) {
-                addRoutesFromCustomConfigs(file);
-            }
-        }
-    }
 }
---
+++ carbon-gw/components/org.wso2.carbon.gateway/src/main/java/org/wso2/carbon/gateway/internal/mediation/camel/CamelConfigWatchAgent.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.gateway.internal.mediation.camel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.gateway.internal.GatewayActivator;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+/**
+ * Camel configuration file watching agent implementation
+ */
+public class CamelConfigWatchAgent {
+    private static final Logger log = LoggerFactory.getLogger(CamelConfigWatchAgent.class);
+
+    private ExecutorService pool = Executors.newFixedThreadPool(10);
+    private int eventCount = 0;
+
+    /**
+     * Start watching the specified directory for file modifications, adding new files, etc
+     *
+     * @param customRouteManger customRouteManger
+     * @throws Exception
+     */
+    public void startWatchingForModifications(CamelCustomRouteManager customRouteManger) throws Exception {
+        Path directoryPath = Paths.get(GatewayActivator.CAMEL_CONFIGS_DIRECTORY);
+        log.info("Start watching directory [Path: " + directoryPath.toString() + "]");
+        try {
+            Future<Integer> future = pool.submit(() -> {
+                try {
+                    Boolean isFolder = (Boolean) Files.getAttribute(directoryPath, "basic:isDirectory", NOFOLLOW_LINKS);
+                    if (!isFolder) {
+                        throw new IllegalArgumentException("Path: " + directoryPath + " is not a folder");
+                    }
+                } catch (IOException ioe) {
+                    throw ioe;
+                }
+
+                FileSystem fs = directoryPath.getFileSystem();
+                try (WatchService service = fs.newWatchService()) {
+                    directoryPath.register(service, ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE, OVERFLOW);
+                    WatchKey key;
+                    while (true) {
+                        log.info("Watch service running ...");
+                        key = service.take();
+                        WatchEvent.Kind<?> kind;
+
+                        log.info("FileWatcher event detected!");
+                        List<WatchEvent<?>> watchEvents = key.pollEvents();
+                        WatchEvent createdEvent = null;
+
+                        for (WatchEvent<?> watchEvent : watchEvents) {
+
+                            String fileName = watchEvent.context().toString();
+
+                            kind = watchEvent.kind();
+                            if (OVERFLOW == kind || fileName.startsWith(".") || fileName.endsWith("~")) {
+                                continue;
+                            } else if (ENTRY_CREATE == kind) {
+
+                                log.info("New File creation Event Fired!");
+                                createdEvent = watchEvent;
+                                customRouteManger.addRoutesFromCustomConfigs(fileName);
+
+                            } else if (ENTRY_DELETE == kind) {
+                                log.info("File deleted Event Fired!");
+                                customRouteManger.removeRoutesFromCustomConfigs(fileName);
+                            } else if (ENTRY_MODIFY == kind) {
+                                if (createdEvent != null) {
+                                    // file creation is non-atomic in some operating systems
+                                    if (!fileName.equals(createdEvent.context().toString())) {
+                                        log.info("File modification Event Fired!");
+                                        customRouteManger.modifyRoutesFromCustomConfigs(fileName);
+                                        createdEvent = null;
+                                    }
+                                } else {
+                                    log.info("File modification Event Fired!");
+                                    customRouteManger.modifyRoutesFromCustomConfigs(fileName);
+                                }
+                            }
+
+                        }
+                        key.reset();
+                    }
+
+                } catch (Exception e) {
+                    throw e;
+                }
+            });
+            if (future == null) {
+                log.error("Camel config watcher has failed!");
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
