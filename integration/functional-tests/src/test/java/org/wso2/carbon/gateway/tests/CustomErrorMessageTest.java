f6b4c17f3ba8fab3ebbc2ed5c384c6813f627a62
Riyafa Abdul Hameed <riyafa@wso2.com>
2016-02-18 10:01:42 +0530

    Test for "GW-7:Define Custom Error Messages"

---
+++ integration/functional-tests/src/test/java/org/wso2/carbon/gateway/tests/CustomErrorMessageTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package org.wso2.carbon.gateway.tests;
+
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.gateway.tests.internal.GWIntegrationTest;
+import org.wso2.gw.emulator.dsl.Emulator;
+import org.wso2.gw.emulator.http.client.contexts.HttpClientConfigBuilderContext;
+import org.wso2.gw.emulator.http.client.contexts.HttpClientRequestBuilderContext;
+import org.wso2.gw.emulator.http.client.contexts.HttpClientResponseBuilderContext;
+import org.wso2.gw.emulator.http.client.contexts.HttpClientResponseProcessorContext;
+
+import java.io.File;
+
+public class CustomErrorMessageTest extends GWIntegrationTest {
+
+    @BeforeClass
+    public void setup() throws Exception {
+        gwDeployArtifacts("artifacts" + File.separator + "camel-context.xml", "/default");
+        gwRestart();
+    }
+
+    @Test
+    public void customErrorMessage() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator().client()
+                .given(HttpClientConfigBuilderContext.configure().host("127.0.0.1").port(9090))
+                .when(HttpClientRequestBuilderContext.request().withPath("/gw/customerGet/1")
+                        .withMethod(HttpMethod.GET)).then(HttpClientResponseBuilderContext.response().assertionIgnore())
+                .operation().send();
+
+        Assert.assertEquals(response.getReceivedResponse().getStatus(), HttpResponseStatus.BAD_GATEWAY,
+                "Expected response code not found");
+        Assert.assertEquals(response.getReceivedResponseContext().getResponseBody(),
+                "<errorMessage>MY Error Message</errorMessage>", "Expected response not found");
+    }
+
+}
